8497bc9196de96c79788c9cf8308043421a15531
kavya-gupta-3 <kavya.ugupta@gmail.com>
2025-06-29 01:51:23 +0530

    Security improvements: Updated .gitignore and service worker config

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,7 +2,13 @@ importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  // ... your config ...
+  apiKey: "AIzaSyBTUUb_y1aP8fiVNXuIC9w-83dyGjg-qDo",
+  authDomain: "birthdayreminder-a5def.firebaseapp.com",
+  projectId: "birthdayreminder-a5def",
+  storageBucket: "birthdayreminder-a5def.firebasestorage.app",
+  messagingSenderId: "858610635830",
+  appId: "1:858610635830:web:1a17972bcb1e439ca43497",
+  measurementId: "G-DQDNPCSJ0R"
 });
 
 const messaging = firebase.messaging();
